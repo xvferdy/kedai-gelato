b8863080e9f4d71c73bb8e8052f950552fd18615
xvferdy <xv.ferdy@gmail.com>
2022-04-29 05:16:53 +0700

    ✨ [feature]: Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 function Footer() {
   return (
     <footer>
-      <h2>footer</h2>
+      <p>
+        Made with 🧡 by{" "}
+        <Link href="https://nextjs.org/" passHref>
+          <a target="_blank" rel="noopener noreferrer">
+            Next JS
+          </a>
+        </Link>
+        , {"  "}
+        <Link href="https://sass-lang.com/" passHref>
+          <a target="_blank" rel="noopener noreferrer">
+            {" "}
+            Sass
+          </a>
+        </Link>{" "}
+        and{" "}
+        <Link href="https://github.com/mui-org/material-ui" passHref>
+          <a target="_blank" rel="noopener noreferrer">
+            Material-UI
+          </a>
+        </Link>
+      </p>
     </footer>
   );
 }
