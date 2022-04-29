ea96ab6edb5ca5977037bae54aee0021489ad65c
xvferdy <xv.ferdy@gmail.com>
2022-04-29 14:01:38 +0700

    ♻️ [refactor]: JSX & scss

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,25 +2,12 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer>
+    <footer className="footer">
       <p>
         Made with 🧡 by{" "}
         <Link href="https://nextjs.org/" passHref>
           <a target="_blank" rel="noopener noreferrer">
             Next JS
-          </a>
-        </Link>
-        , {"  "}
-        <Link href="https://sass-lang.com/" passHref>
-          <a target="_blank" rel="noopener noreferrer">
-            {" "}
-            Sass
-          </a>
-        </Link>{" "}
-        and{" "}
-        <Link href="https://github.com/mui-org/material-ui" passHref>
-          <a target="_blank" rel="noopener noreferrer">
-            Material-UI
           </a>
         </Link>
       </p>
--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,23 @@ import Link from "next/link";
 export default function Home() {
   const dummyProducts = [
     {
-      name: "oreo",
+      img: "/assets/pr1.png",
+      type: "original",
+      name: "DAIFUKU CORNETTO",
       desc: " Lorem ipsum dolor sit amet consectetur at consectetur  t consectetur  t consectetur dipisicing el  Lorem ipsum dolor sit amet consectetur adipisicing el",
     },
+
     {
-      name: "mocha",
-      desc: " Lorem ipsumsectetur adipisicing el",
+      img: "/assets/pr6.png",
+      type: "nature",
+      name: "GO GREEN (TEA)",
+      desc: " Lorem ipsum dolor s ipsum dolor sit amet consectetur adipisicing el",
     },
     {
-      name: "greentea",
-      desc: " Lorem ipsum dolor s ipsum dolor sit amet consectetur adipisicing el",
+      img: "/assets/pr2.png",
+      type: "coffee",
+      name: "GOOD DAY ABANGAN",
+      desc: " Lorem ipsumsectetur adipisicing el",
     },
   ];
 
@@ -31,19 +38,19 @@ export default function Home() {
 
       <section id="home" className="home">
         <div className="title">
-          <h1>Kedai Gelato</h1>
-          <small>100% Natural</small>
+          {/* <h1>Kedai Gelato</h1> */}
+          <p>100% Modern Taste!</p>
           <h2>Products</h2>
         </div>
         <div className="container home__container">
           {dummyProducts.map((product) => (
-            <div className={`home__product home__product--${product.name}`}>
+            <div className={` home__product--${product.type}`}>
               <div className="home__product-image">
                 <Image
-                  src="/assets/pr1.png"
+                  src={product.img}
                   width={200}
                   height={280}
-                  alt="Ice Cream"
+                  alt={product.name}
                 />
               </div>
               <h3>{product.name}</h3>
